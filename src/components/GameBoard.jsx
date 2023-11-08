856fb35767c327b323d4ae73214b458387a37a6b
shafiquddin <shafiquddin2k@gmail.com>
2023-11-08 03:30:21 -0800

    button working properly

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -5,11 +7,22 @@ const initialGameBoard = [
 ];
 
 const GameBoard = () => {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    const onClickBoardHandler = (rowIndex, colIndex) => {
+        setGameBoard((preGameBoard) => {
+            const updateBoard = [...preGameBoard.map(innerArray => [...innerArray])];
+            updateBoard[rowIndex][colIndex] = 'X';
+            return updateBoard;
+        }
+        )
+    }
+
     return <ol id="game-board">
-        {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
+        {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
             <ol>
                 {row.map((playerSymbol, colIndex) => <li key={colIndex}>
-                    <button>{playerSymbol}</button>
+                    <button onClick={() => onClickBoardHandler(rowIndex, colIndex)}>{playerSymbol}</button>
                 </li>)}
             </ol>
         </li>)}
